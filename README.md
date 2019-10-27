44ff78691e987f013d316bdebc24026b9ad06396
Scott Fleming <scott.s.fleming@gmail.com>
2019-10-27 09:30:01 -0500

    listening time formated with padded 0s

--- README.md
+++ README.md
@@ -4,6 +4,5 @@
 Edit `config.sample.json` and save it as `config.json` which contain the credentials for your shoutcast server and mongodb configuration.
 
 `npm run install`
-
 `npm run start` 
 
--- index.js
+++ index.js
@@ -4,43 +4,58 @@ const geoip = require('geo-from-ip');
 const mongo = require('mongodb');
 const config = require('./config.json');
 const sc = config.shoutcast; 
-var mc = config.mongodb;
+const mc = config.mongodb;
 const MongoUrl = `mongodb://${mc.host}:${mc.port}/${mc.db}`;
+const myCollection = mc.collection;
+const database = mc.db;
+
+const dt = new Date();
+    console.log(dt);
 
 
 function runner(){
 
 getJSON(sc.url + "/" + sc.path + "?sid=" + sc.sid + '&mode=' + sc.mode + '&pass=' + sc.pass)
   .then(function(resp) {
-    console.log('\033[2J');
-    console.log(`Server Uptime: ${secondsToHms(resp.streamuptime)}`)
-          
+    
+    let server = Object.assign({}, resp); // clone resp so we don't kill the object when deleting listener and songs
+    var serverStat = getserverStats(server);
+
+    console.log('\033[2J'); // clear console 
+    console.log(`Server Uptime: ${secondsToHms(resp.streamuptime)}`);
+    console.log(`Currently playing: ${resp.songtitle}`);
+    
+    // Got milk? Then spill it!
     if( resp.uniquelisteners > 0 ) {
       
-      console.log( `You have ${resp.uniquelisteners} unique listeners:` );
+      console.log(`Average Listening Time: ${secondsToHms(serverStat.averagetime)}`);
+      console.log( `Listeners: ${resp.uniquelisteners}` );
+      console.log( `--------------\n`);
       
       var listeners = getListeners(resp);
+
       for( let listener of listeners){
+
         listener.geo = geoip.allData(listener.hostname);
         listener.connecttime = secondsToHms(listener.connecttime);
         listener.referer = listener.referer == '' ? 'DNAS' : 'HTML'
-
+        listener.timestamp = dt.toLocaleString();
   
-        console.log(listener.hostname + ' ' 
-        + listener.geo.city +  ', ' + listener.geo.state + ' ' + listener.geo.code.country + ' '
-        + listener.connecttime + ' ' + listener.referer + ' ' + listener.useragent
+        // Display listener info line
+        console.log( listener.timestamp + ' | ' + listener.hostname + ' | ' 
+        + listener.geo.city +  ', ' + listener.geo.state + ' ' + listener.geo.code.country + ' | '
+        + listener.connecttime + ' | ' + listener.referer + ' | ' + listener.geo.location.latitude + ',' + listener.geo.location.longitude
         );
 
-        // now update the mongo table
+        // now update the mongo table with the listener data
         mongoUpdate(listener);
 
       }
       
     }else{
-      console.log(`There are ${resp.uniquelisteners} listeners :(`  )
+      console.log(`Sorry, but there are ${resp.uniquelisteners} listeners at the moment :(`  );
     }
     
-    console.log(`Currently: ${resp.songtitle}`);
     
   }).catch(function(error){
     console.log(error);
@@ -50,26 +65,28 @@ getJSON(sc.url + "/" + sc.path + "?sid=" + sc.sid + '&mode=' + sc.mode + '&pass=
 runner();
 
 // loop it every X milliseconds 60000 = 1 minute
-setInterval(runner, 60000);
+setInterval(runner, 15000);
   
+// --------- FUNCTION FUNCTION, WHAT'S YOUR FUNCTION?? ------------
 
-function getListeners(d){
-  return d.listeners;
+function getListeners(d){ return d.listeners; }
+
+function getserverStats(s){
+  delete(s.listeners);
+  delete(s.songs);
+  return s;
 }
 
-function secondsToHms(d) {
-  d = Number(d);
-  var h = Math.floor(d / 3600);
-  var m = Math.floor(d % 3600 / 60);
-  var s = Math.floor(d % 3600 % 60);
-
-  var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-  var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-  // return hDisplay + mDisplay + sDisplay; 
+function secondsToHms(t) {
+  t     = Number(t);
+  var h =  pad(Math.floor(t / 3600));
+  var m =  pad(Math.floor(t % 3600 / 60));
+  var s = pad(Math.floor(t % 3600 % 60));
+  
   return h + ":" + m + ":" + s;
-
 }
+
+function pad(n){ return n < 10 ? '0'+n : n }
 
 function mongoUpdate(data){
   mongo.connect(MongoUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
@@ -77,34 +94,37 @@ function mongoUpdate(data){
        console.log(err);
        process.exit(0);
     }
-    // console.log('Mongo connected!');
-    var dbo = db.db('hawkwyndRadio');
-    var collection = dbo.collection('listeners');
+    // connect to database, and set collection (config.json)
+    var dbo        = db.db( database );
+    var collection = dbo.collection( myCollection );
 
-    // console.log(data);
-
-    // do update
+    // do update/insert of our record
     try{
       collection.findOneAndUpdate(
          {"hostname" : data.hostname},
          {$set: {
+             "timestamp" : data.timestamp,
              "referer" : data.referer,
              "useragent" : data.useragent,
              "hostname" : data.hostname,
+             "lat"      : data.geo.location.latitude,
+             "lng"  : data.geo.location.longitude,
              "country" : data.geo.country,
              "city" : data.geo.city,
              "state" : data.geo.state,
              "connecttime" : data.connecttime
          },
      },
-     { upsert:true, returnNewDocument : true }
+     { upsert:true, returnNewDocument : true } // upsert will insert if not found
          
       );
   }
   catch(e){
+      // spew error if we have one
       console.log(e);
+      process.exit(0);
   }
-    
+   // shut the door on your way out.
   db.close();
 
   });
